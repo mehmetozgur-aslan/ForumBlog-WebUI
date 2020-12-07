445643aae370d5917c2c31ff7a0d974e6d0a9ec6
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-07 20:15:19 +0300

    Detail sayfasına resmin getirilmesi

---
+++ ForumBlog.Web/Enums/BlogImageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.Enums
+{
+    public enum BlogImageType
+    {
+        BlogHome = 1,
+        BlogDetail = 2
+    }
+}
--- ForumBlog.Web/TagHelpers/ImageTagHelper.cs
+++ ForumBlog.Web/TagHelpers/ImageTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using ForumBlog.Web.ApiServices.Interfaces;
+using ForumBlog.Web.Enums;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
@@ -18,12 +19,23 @@ namespace ForumBlog.Web.TagHelpers
         }
 
         public int Id { get; set; }
+        public BlogImageType BlogImageType { get; set; } = BlogImageType.BlogHome;
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             var blob = await _imageApiService.GetBlogImageByIdAsync(Id);
 
-            var html = $"<img src='{blob}' class='card-img-top' />";
+            string html = string.Empty;
+
+            if (BlogImageType == BlogImageType.BlogHome)
+            {
+                html = $"<img src='{blob}' class='card-img-top' />";
+            }
+            else
+            {
+                html = $"<img src='{blob}' class='img-fluid rounded' />";
+            }
+
             output.Content.SetHtmlContent(html);
         }
     }
