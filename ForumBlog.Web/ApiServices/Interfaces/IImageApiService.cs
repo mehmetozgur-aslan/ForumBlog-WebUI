7a9dfdfb4ed8e96b15362515126f673d56f4c6fe
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-07 15:21:06 +0300

    resim dosyasının apiden çekilmesi

---
+++ ForumBlog.Web/ApiServices/Interfaces/IImageApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.ApiServices.Interfaces
+{
+    public interface IImageApiService
+    {
+        Task<string> GetBlogImageByIdAsync(int id);
+    }
+}
