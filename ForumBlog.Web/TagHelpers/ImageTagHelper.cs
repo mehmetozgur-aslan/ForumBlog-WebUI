6c2e306c0430cf419187c9f386162a4f86986d2d
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-07 15:42:37 +0300

    TagHelper ile resimlerin gösterilmesi

---
+++ ForumBlog.Web/TagHelpers/ImageTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using ForumBlog.Web.ApiServices.Interfaces;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.TagHelpers
+{
+    [HtmlTargetElement("getblogimage")]
+    public class ImageTagHelper : TagHelper
+    {
+        private readonly IImageApiService _imageApiService;
+
+        public ImageTagHelper(IImageApiService imageApiService)
+        {
+            _imageApiService = imageApiService;
+        }
+
+        public int Id { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var blob = await _imageApiService.GetBlogImageByIdAsync(Id);
+
+            var html = $"<img src='{blob}' class='card-img-top' />";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
