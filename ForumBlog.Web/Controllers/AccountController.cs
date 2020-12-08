c0502f303a80fc0a6ab04c6374aaaf9006d6fcce
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-08 20:08:12 +0300

    Kullanıcı giriş ekranının tasarlanması

---
+++ ForumBlog.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using ForumBlog.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(AppUserLoginModel appUserLoginModel)
+        {
+            return View();
+        }
+    }
+}
