5a953f71d60e858c03aa9b9d499e32fdd270fa62
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-09 14:34:44 +0300

    JwtHelper

---
+++ ForumBlog.Web/Extensions/CustomSessionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.Extensions
+{
+    public static class CustomSessionExtension
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            var jsonData = JsonConvert.SerializeObject(value);
+            session.SetString(key, jsonData);
+        }
+
+        public static T GetObject<T>(this ISession session, string key) where T : class, new()
+        {
+            var data = session.GetString(key);
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+}
