4cea1b877032d41f8735fbc5701422875db3d9eb
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-09 15:21:20 +0300

    blogların listelenmesi ve blogaddmodel'in oluşturulması

---
+++ ForumBlog.Web/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using ForumBlog.Web.ApiServices.Interfaces;
+using ForumBlog.Web.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        private readonly IBlogApiService _blogApiService;
+
+        public BlogController(IBlogApiService blogApiService)
+        {
+            _blogApiService = blogApiService;
+        }
+
+        [JwtAuthorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _blogApiService.GetAllAsync());
+        }
+    }
+}
---
+++ ForumBlog.Web/Models/BlogAddModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.Models
+{
+    public class BlogAddModel
+    {
+        public int AppUserId { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
