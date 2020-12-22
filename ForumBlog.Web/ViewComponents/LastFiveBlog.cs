ae666f89fd386ac434b47389324262dbad23713f
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-22 11:56:41 +0300

    Son 5 blog getirilmesi

---
+++ ForumBlog.Web/ViewComponents/LastFiveBlog.cs
@@ -0,0 +1,24 @@
+﻿using ForumBlog.Web.ApiServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.Web.ViewComponents
+{
+    public class LastFiveBlog : ViewComponent
+    {
+        private readonly IBlogApiService _blogApiService;
+
+        public LastFiveBlog(IBlogApiService blogApiService)
+        {
+            _blogApiService = blogApiService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_blogApiService.GetLastFiveAsync().Result);
+        }
+    }
+}
